954255257b287ad4cfb8d5381fdef4ef08b5ec05
Lucas Mustafa <luskas.mustafa@hotmail.com>
2025-02-17 21:02:54 -0300

    autores

---
+++ dados.js
@@ -0,0 +1,80 @@
+let dados = [
+    {
+        titulo: "Machado de Assis",
+        descricao: "Principal nome do Realismo brasileiro, autor de 'Memórias Póstumas de Brás Cubas' e 'Dom Casmurro'. Fundador da Academia Brasileira de Letras.",
+        link: "https://pt.wikipedia.org/wiki/Machado_de_Assis",
+        tags: "realismo literatura brasileira Academia Brasileira de Letras romancista 1839-1908"
+    },
+    {
+        titulo: "Aluísio Azevedo",
+        descricao: "Expoente do Naturalismo brasileiro com 'O Cortiço', retratando a vida urbana e questões sociais no Rio Imperial.",
+        link: "https://pt.wikipedia.org/wiki/Alu%C3%ADsio_Azevedo",
+        tags: "naturalismo O Cortiço literatura social 1857-1913"
+    },
+    {
+        titulo: "Raul Pompeia",
+        descricao: "Autor de 'O Ateneu', romance realista-naturalista que critica a sociedade burguesa através de um internato escolar.",
+        link: "https://pt.wikipedia.org/wiki/Raul_Pompeia", 
+        tags: "O Ateneu crítica social prosa psicológica 1863-1895"
+    },
+    {
+        titulo: "Júlia Lopes de Almeida",
+        descricao: "Pioneira na literatura realista feminina, autora de 'A Falência' que aborda transformações econômicas e familiares.",
+        link: "https://pt.wikipedia.org/wiki/J%C3%BAlia_Lopes_de_Almeida",
+        tags: "realismo feminino A Falência sociedade patriarcal 1862-1934"
+    },
+    {
+        titulo: "Visconde de Taunay",
+        descricao: "Autor de 'Inocência', romance regionalista que combina elementos românticos com observação realista do sertão.",
+        link: "https://pt.wikipedia.org/wiki/Alfredo_d%27Escragnolle_Taunay",
+        tags: "romance regionalista Inocência transição romantismo-realismo 1843-1899"
+    },
+    {
+        titulo: "Adolfo Caminha",
+        descricao: "Representante do Naturalismo com 'Bom-Crioulo', abordando temas como homossexualidade e determinismo social.",
+        link: "https://pt.wikipedia.org/wiki/Adolfo_Caminha",
+        tags: "naturalismo Bom-Crioulo literatura polêmica 1867-1897"
+    },
+    {
+        titulo: "Inglês de Sousa",
+        descricao: "Autor de 'O Missionário', importante romance realista que critica a instituição religiosa na Amazônia.",
+        link: "https://pt.wikipedia.org/wiki/Herculano_Marcos_Ingl%C3%AAs_de_Sousa",
+        tags: "O Missionário crítica religiosa realismo amazônico 1853-1918"
+    },
+    {
+        titulo: "Domingos Olímpio",
+        descricao: "Autor de 'Luzia-Homem', romance naturalista que retrata os costumes e conflitos do sertão nordestino.",
+        link: "https://pt.wikipedia.org/wiki/Domingos_Ol%C3%ADmpio", 
+        tags: "naturalismo Luzia-Homem regionalismo 1850-1906"
+    },
+    {
+        titulo: "Manuel Antônio de Almeida",
+        descricao: "Precursor do Realismo com 'Memórias de um Sargento de Milícias', retrato realista da sociedade carioca no século XIX.",
+        link: "https://pt.wikipedia.org/wiki/Manuel_Ant%C3%B4nio_de_Almeida",
+        tags: "pré-realismo sociedade carioca sátira social 1831-1861"
+    },
+    {
+        titulo: "Franklin Távora",
+        descricao: "Autor regionalista que transitou entre Romantismo e Realismo, conhecido por 'O Cabeleira' sobre o cangaço nordestino.",
+        link: "https://pt.wikipedia.org/wiki/Franklin_T%C3%A1vora", 
+        tags: "regionalismo transição romantismo-realismo literatura nordestina 1842-1888"
+    },
+    {
+        titulo: "Valentim Magalhães",
+        descricao: "Importante divulgador do Realismo no Brasil, autor de contos e romances com crítica às elites urbanas.",
+        link: "https://pt.wikipedia.org/wiki/Valentim_Magalh%C3%A3es",
+        tags: "realismo contos crítica social 1859-1903"
+    },
+    {
+        titulo: "Euclides da Cunha",
+        descricao: "Autor de 'Os Sertões', obra monumental que combina análise científica e prosa realista sobre a Guerra de Canudos.",
+        link: "https://pt.wikipedia.org/wiki/Euclides_da_Cunha",
+        tags: "pré-modernismo Os Sertões análise social 1866-1909"
+    },
+    {
+        titulo: "João do Rio",
+        descricao: "Cronista realista que retratou o Rio de Janeiro da Belle Époque em obras como 'A Alma Encantadora das Ruas'.",
+        link: "https://pt.wikipedia.org/wiki/Jo%C3%A3o_do_Rio",
+        tags: "realismo urbano crônicas jornalismo literário 1881-1921"
+    }
+];
